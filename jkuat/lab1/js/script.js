07a2f56f173fcea1bdece26896c3d03b8a14e980
OpenMaps <mwasjos@users.noreply.github.com>
2017-04-24 06:23:47 +0000

    Update script.js
    location

--- jkuat/lab1/js/script.js
+++ jkuat/lab1/js/script.js
@@ -48,6 +48,22 @@ var schoolpoints = L.geoJson(schools, {
 	}
 }).addTo(map);
 */
+//Geolocation
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+    L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
 
 /*
 function showCoords(e) {
